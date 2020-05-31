15869fd7eabd6b371280d2546bcefdc65d2a7eb3
Balazs Kreith <balazskreith@users.noreply.github.com>
2020-05-31 08:16:21 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -64,7 +64,7 @@ cp $DEVOPS_BUILD/used_profiles/${CONFIGURATION_KEY}.properties used-profiles.pro
 cp $DEVOPS_BUILD/micronaut_application_yaml/${CONFIGURATION_KEY}.yaml $SERVICE/src/main/resources/application.yaml
 cp $DEVOPS_BUILD/db_scripts/${SERVICE}/${DATASOURCE}.sql init.sql
 
-docker-compose up $DATASOURCE -d
+docker-compose up -d $DATASOURCE 
 sleep 10
 
 ./gradlew clean
