ddc53818d208091f25f4b0d16308455680a2eb61
Balazs Kreith <balazs.kreith@finago.com>
2020-08-23 08:32:16 +0300

    fix repo

--- service/src/main/java/org/observertc/webrtc/service/repositories/PeerConnectionsRepository.java
+++ service/src/main/java/org/observertc/webrtc/service/repositories/PeerConnectionsRepository.java
@@ -235,7 +235,8 @@ public class PeerConnectionsRepository {
 		return this.contextProvider.get()
 				.selectFrom(TABLE)
 				.where(TABLE.BROWSERID.eq(browserID))
-				.and(TABLE.JOINED.eq(joined))
+				.and(TABLE.JOINED.gt(joined.minusSeconds(1)))
+				.and(TABLE.JOINED.lt(joined.plusSeconds(1)))
 				.limit(1)
 				.fetchOptional();
 	}
