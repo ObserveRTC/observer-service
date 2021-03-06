bf668377c478315ff5aae985d075be988712c03a
Balazs Kreith <balazskreith@users.noreply.github.com>
2021-03-06 14:54:18 +0200

    Add Authentication support (#43)
    * Add dependencies to gradle to support OAuth2
     * Add a simple home page to redirect into the right login page
     * Add examples into application yaml to give AWS, Google and Okta configuration to oauth endpoint (was not very hard though)

--- src/main/java/org/observertc/webrtc/observer/controllers/IndexController.java
+++ src/main/java/org/observertc/webrtc/observer/controllers/IndexController.java
@@ -1,17 +1,13 @@
 package org.observertc.webrtc.observer.controllers;///*
 
-import com.hazelcast.map.IMap;
 import io.micronaut.http.HttpStatus;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.rules.SecurityRule;
-import org.observertc.webrtc.observer.ObserverConfig;
-import org.observertc.webrtc.observer.ObserverHazelcast;
+import io.micronaut.views.View;
 
-import javax.inject.Inject;
-import javax.inject.Named;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 
 @Secured(SecurityRule.IS_ANONYMOUS)
@@ -27,4 +23,10 @@ public class IndexController {
 		return HttpStatus.OK;
 	}
 
+	@Secured(SecurityRule.IS_ANONYMOUS)
+	@View("home")
+	@Get("/home")
+	public Map<String, Object> getHome() {
+		return new HashMap<>();
+	}
 }
