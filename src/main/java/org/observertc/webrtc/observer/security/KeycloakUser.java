75d2f827316fb783465d21e7a894909d26088627
Balazs Kreith <kreith.balazs@gmail.com>
2021-04-24 14:05:57 +0300

    keycloak

---
+++ src/main/java/org/observertc/webrtc/observer/security/KeycloakUser.java
@@ -0,0 +1,10 @@
+package org.observertc.webrtc.observer.security;
+
+import java.util.List;
+
+public class KeycloakUser {
+    public String email;
+    public String username;
+    public List<String> roles;
+
+}
