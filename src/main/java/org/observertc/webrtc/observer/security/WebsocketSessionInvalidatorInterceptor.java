b3ff7bed339ef83619031c13fc550002237a7075
Balazs Kreith <kreith.balazs@gmail.com>
2021-05-17 08:11:12 +0300

    add invalidator to avoid overloading

--- src/main/java/org/observertc/webrtc/observer/security/WebsocketSessionInvalidatorInterceptor.java
+++ src/main/java/org/observertc/webrtc/observer/security/WebsocketSessionInvalidatorInterceptor.java
@@ -3,14 +3,19 @@ package org.observertc.webrtc.observer.security;
 import io.micronaut.aop.MethodInterceptor;
 import io.micronaut.aop.MethodInvocationContext;
 import io.micronaut.websocket.WebSocketSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
 @Singleton
-public class WebsocketSessionValidatorInterceptor implements MethodInterceptor<Object, Object> {
-    public WebsocketSessionValidatorInterceptor() {
+public class WebsocketSessionInvalidatorInterceptor implements MethodInterceptor<Object, Object> {
+    private static final Logger logger = LoggerFactory.getLogger(WebsocketSessionInvalidatorInterceptor.class);
+    private static final String ACCESS_TOKEN_HOLDER_PARAMETER_NAME = "accessToken";
+
+    public WebsocketSessionInvalidatorInterceptor() {
 
     }
 
@@ -27,14 +32,13 @@ public class WebsocketSessionValidatorInterceptor implements MethodInterceptor<O
         if (!sessionValidator.isEnabled()) {
             return context.proceed();
         }
-        WebSocketSession session = WebsocketSessionValidator.extractWebsocketSession(context, ValidateWebsocketSession.class);
+        WebSocketSession session = WebsocketSessionValidator.extractWebsocketSession(context, InvalidateWebsocketSession.class);
         if (sessionValidator.isEnabled()) {
-            boolean valid = sessionValidator.isValid(session);
-            if (!valid) {
-                return null;
-            }
+            sessionValidator.makeInvalid(session);
         }
         Object result = context.proceed();
         return result;
     }
+
+
 }
